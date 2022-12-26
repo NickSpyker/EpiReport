580d5a7ca36305c0525fd275079408dc97d5e238
Nicolas Spijkerman <nicolas.spijkerman@epitech.eu>
2022-12-26 12:30:23 +0100

    dev docker handler

---
+++ src/docker.rs
@@ -0,0 +1,51 @@
+use std::{
+    path::PathBuf,
+    fs::{remove_file, read_to_string},
+    process::Command
+};
+
+pub fn get_coding_style_reports(path: PathBuf) -> Result<String, String>
+{
+    let reports_file_path: String = format!("{}/coding-style-reports.log", path.display());
+
+    remove_file(&reports_file_path).unwrap_or(());
+
+    match Command::new("sudo").args([
+        "docker",
+        "pull",
+        "ghcr.io/epitech/coding-style-checker:latest"
+    ]).output() {
+        Ok(output) => println!("{}", String::from_utf8_lossy(&output.stdout)),
+        Err(_) => return Err("ERREUR : Impossible de telecharger l image Docker !".to_string())
+    }
+
+    match Command::new("sudo").args([
+        "docker",
+        "image",
+        "prune",
+        "-f"
+    ]).output() {
+        Ok(output) => println!("{}", String::from_utf8_lossy(&output.stdout)),
+        Err(_) => return Err("ERREUR : Impossible de supprimer les anciennes versions de l image Docker".to_string())
+    }
+
+    match Command::new("sudo").args([
+        "docker",
+        "run",
+        "--rm",
+        "-i",
+        "-v",
+        &format!("{}:/mnt/delivery", path.display()),
+        "-v",
+        &format!("{}:/mnt/reports", path.display()),
+        "ghcr.io/epitech/coding-style-checker:latest",
+        "/mnt/delivery",
+        "/mnt/reports"
+    ]).output() {
+        Ok(_) => match read_to_string(reports_file_path) {
+            Ok(buffer) => Ok(buffer),
+            Err(_) => Err("ERREUR : Impossible de lire les rapports !".to_string())
+        },
+        Err(_) => Err("ERREUR : Impossible de generer les rapports !".to_string())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,22 @@
-fn main()
+mod docker;
+
+use std::{
+    path::{PathBuf, Path},
+};
+
+fn main() -> Result<(), String>
 {
-    println!("Hello, world!");
+    let current_directory: PathBuf = match Path::new(".").canonicalize() {
+        Ok(dir) => {
+            println!("Chemin du repertoire courant : {}", dir.display());
+            dir
+        },
+        Err(_) => return Err("ERREUR : Impossible de recuperer le chemin du repertoire courant !".to_string())
+    };
+
+    let buffer: String = docker::get_coding_style_reports(current_directory)?;
+
+    println!("{buffer}");
+
+    Ok(())
 }
